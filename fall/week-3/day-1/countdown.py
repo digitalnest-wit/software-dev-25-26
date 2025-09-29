5a83ecb85d16214e0b9b5ad6eaff8775b8c18a9b
Leonardo Villalobos <villaleobos@gmail.com>
2025-09-29 14:50:51 -0700

    add w3/d1/...

---
+++ fall/week-3/day-1/countdown.py
@@ -0,0 +1,11 @@
+n = int(input('Enter a number: '))
+
+# Counting down with a while loop
+counter = n
+while counter > 0:
+    print(counter)
+    counter -= 1
+
+# Counting down with a for loop
+# for i in range(n, 0, -1):
+#     print(i)
---
+++ fall/week-3/day-1/sum.py
@@ -0,0 +1,7 @@
+n = int(input('Enter a number: '))
+
+result = 0
+for i in range(n):
+    result += i
+
+print(f'Sum: {result}')
